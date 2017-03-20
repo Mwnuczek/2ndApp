c1f2274c004a0c1dad62d91839767ec859b85295
Mwnuczek <mirekw8@gmail.com>
2017-03-20 12:29:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/packard/a2ndapp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.packard.a2ndapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String tag="1st tag";
+
+    private static Button button;
+    private static Button button2;
+    private static Button button3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText("Wciśnięto przycisk1");
+            }
+        });
+        button2 = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText("Wciśnięto przycisk2");
+            }
+        });
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText("Wciśnięto przycisk3");
+            }
+        });
+
+
+        Log.d(tag,"Create log");
+    }
+}
